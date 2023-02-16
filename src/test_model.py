933e5b5b35bd17d0a53992e3f400cde0ba2611ec
Bruno Alvisio <bruno@bioteam.net>
2023-02-16 11:33:09 -0800

    Formatted test_model.py

--- src/test_model.py
+++ src/test_model.py
@@ -1,51 +1,53 @@
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import numpy as np
-from PIL import Image
 import os
-from models import generator,cnn_discriminator
 
-image_shape  = (200,300,1)
-path_to_test_data = "./data/test_data/input"
-path_to_results = './data/test_data/result'
-path_to_trained_model = './data/trained_model/trained_model_weights.h5' 
+import numpy as np
+from pathlib import Path
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# loading the trained model
+from models import generator, cnn_discriminator
 
-d_model = cnn_discriminator(image_shape)
+IMAGE_SHAPE = (200, 300, 1)
+PATH_TO_TEST_DATA = "./data/test_data/input"
+PATH_TO_RESULTS = "./data/test_data/result"
+PATH_TO_TRAINED_MODEL = "./data/trained_model/trained_model_weights.h5"
 
-g_model = generator(image_shape)
 
-g_model.load_weights(path_to_trained_model ,by_name=True)
-
-# loading the test dataset
 def load_real_samples(X):
-	X = X.astype('float32')
-	# scale from [0,255] to [-1,1]
-	X = (X - 127.5) / 127.5
-	return X
+    X = X.astype("float32")
+    # scale from [0,255] to [-1,1]
+    X = (X - 127.5) / 127.5
+    return X
 
-train_datagen = ImageDataGenerator(preprocessing_function=load_real_samples)
 
-train_generator_1 = train_datagen.flow_from_directory(
-    directory = path_to_test_data,
-    target_size = (image_shape[0],image_shape[1]),
-    batch_size = 1,
-    color_mode = "grayscale",
-    class_mode = None,
-    shuffle = False,
-    seed = 42
-    
-) 
+if __name__ == "__main__":
+    # loading the trained model
+    d_model = cnn_discriminator(IMAGE_SHAPE)
+    g_model = generator(IMAGE_SHAPE)
+    g_model.load_weights(PATH_TO_TRAINED_MODEL, by_name=True)
 
-batch_count=train_generator_1.n 
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=load_real_samples
+    )
 
-fileNames=train_generator_1.filenames
+    train_generator_1 = train_datagen.flow_from_directory(
+        directory=PATH_TO_TEST_DATA,
+        target_size=(IMAGE_SHAPE[0], IMAGE_SHAPE[1]),
+        batch_size=1,
+        color_mode="grayscale",
+        class_mode=None,
+        shuffle=False,
+        seed=42,
+    )
 
-for val in range(batch_count):
-    temp_img=train_generator_1.next()
-    nameF=fileNames[val].split('\\')[1]
-    gen_img = g_model.predict(temp_img).squeeze()
-    gen_img = (gen_img - gen_img.min()) / (gen_img.max() - gen_img.min())
-    result = Image.fromarray((gen_img * 255).astype(np.uint8))
-    result.save(os.path.join(path_to_results, nameF))
+    batch_count = train_generator_1.n
 
+    fileNames = train_generator_1.filenames
+
+    for val in range(batch_count):
+        temp_img = train_generator_1.next()
+        nameF = Path(fileNames[val]).name
+        gen_img = g_model.predict(temp_img).squeeze()
+        gen_img = (gen_img - gen_img.min()) / (gen_img.max() - gen_img.min())
+        result = Image.fromarray((gen_img * 255).astype(np.uint8))
+        result.save(os.path.join(PATH_TO_RESULTS, nameF))
